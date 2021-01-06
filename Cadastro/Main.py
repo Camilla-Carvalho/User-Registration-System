93ce51c5579c612059a8a437d250f1e70ef3a383
Crowmilla <camilla.workd2@gmail.com>
2021-01-06 12:52:58 -0300

    Update Main.py
    Adição de parâmetro da função header; Atualização no modo de exibição do formulário.

--- Cadastro/Main.py
+++ Cadastro/Main.py
@@ -1,17 +1,17 @@
-def header():
-    print('==' * 26)
-    print(f' {"C A D A S T R O":^52} ')
-    print('==' * 26)
+def header(txt):
+    print('==' * 23)
+    print(f' {txt:^46} ')
+    print('==' * 23)
 
 
 def lin():
     print('--' * 26)
 
-
+    
 dados = {}
 ficha = []
 
-header()
+header('C A D A S T R O')
 while True:
     dados.clear()
     try:
@@ -21,16 +21,18 @@ while True:
             dados['nome'] = input('Digite somente letras: ').capitalize()
     else:
         while dados['nome'] == '':
-            dados['nome'] = input('Preencha todos os campos: ').capitalize()
+            dados['nome'] = input('Digite um nome válido: ').capitalize()
             if dados['nome'].isnumeric():
                 dados['nome'] = input('Digite somente letras: ').capitalize()
+
+    dados['email'] = input('Email: ')
+
 
     try:
         dados['idade'] = int(input('Idade: '))
     except Exception as error:
         while ValueError:
-            dados['idade'] = input(
-                '\033[31mERRO!(Digite um valor válido).\033[0m\nIdade:')
+            dados['idade'] = input('\033[31mERRO!(Digite um valor válido).\033[0m\nIdade:')
 
     else:
         if dados['idade'] <= 0:
@@ -51,8 +53,12 @@ while True:
     while option not in 'SN':
         if option not in 'SN':
             print('\033[31mOPÇÃO INVÁLIDA! ', end='')
-        option = input('Deseja continuar?[S/N]: \033[0m').strip().upper()[0]
+        option = input('Deseja continuar?[S/N]: \033[0m\n\n').strip().upper()[0]
     if option in 'N':
         break
 
-print(ficha)
+header('F O R M U L Á R I O')
+for i in enumerate(ficha):
+    for key, value in dados.items():
+        print(f'{key}: {value}')
+    print('--' * 23)
