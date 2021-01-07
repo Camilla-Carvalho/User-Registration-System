88c28f416457cbbfd66bb446985961e179d89436
Crowmilla <camilla.workd2@gmail.com>
2021-01-07 10:19:17 -0300

    Code upgrade

--- Cadastro/Main.py
+++ Cadastro/Main.py
@@ -5,9 +5,9 @@ def header(txt):
 
 
 def lin():
-    print('--' * 26)
+    print('--' * 23)
 
-    
+
 dados = {}
 ficha = []
 
@@ -27,33 +27,34 @@ while True:
 
     dados['email'] = input('Email: ')
 
-
     try:
         dados['idade'] = int(input('Idade: '))
     except Exception as error:
         while ValueError:
-            dados['idade'] = input('\033[31mERRO!(Digite um valor válido).\033[0m\nIdade:')
+            dados['idade'] = input(
+                'ERRO!(Digite um valor válido).\nIdade:')
 
     else:
         if dados['idade'] <= 0:
-            dados['idade'] = input('\033[31mDigite uma idade válida: ')
+            dados['idade'] = input('Digite uma idade válida: ')
 
     try:
-        dados['sexo'] = input('Sexo[F/M]: \033[0m').strip().upper()[0]
+        dados['sexo'] = input('Sexo[F/M]: ').strip().upper()[0]
         if dados not in 'FM':
             pass
     except:
         while dados['sexo'] not in 'FM':
-            print('\033[31mOpção inválida! ', end='')
-            dados['sexo'] = input('Sexo[F/M]: \033[0m').strip().upper()[0]
+            print('Opção inválida! ', end='')
+            dados['sexo'] = input('Sexo[F/M]: ').strip().upper()[0]
     finally:
         ficha.append(dados.copy())
 
     option = input('Deseja continuar?[S/N]: ').strip().upper()[0]
     while option not in 'SN':
         if option not in 'SN':
-            print('\033[31mOPÇÃO INVÁLIDA! ', end='')
-        option = input('Deseja continuar?[S/N]: \033[0m\n\n').strip().upper()[0]
+            print('OPÇÃO INVÁLIDA! ', end='')
+        option = input('Deseja continuar?[S/N]: \n\n').strip().upper()[0]
+    lin()
     if option in 'N':
         break
 
@@ -61,4 +62,4 @@ header('F O R M U L Á R I O')
 for i in enumerate(ficha):
     for key, value in dados.items():
         print(f'{key}: {value}')
-    print('--' * 23)
+    lin()
